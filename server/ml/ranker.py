bef2da0b47273c347c8e6cd097fd3c070d6b4ba1
Benny Huang <37185367+JBennyHuang@users.noreply.github.com>
2021-01-15 23:13:58 -0500

    adding ranker to backend

---
+++ server/ml/ranker.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import argparse
+import torch
+import torchvision.models
+import torchvision.transforms as transforms
+
+class Ranker:
+    def __init__(self):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        self.model = torchvision.models.resnet50()
+        self.model.fc = torch.nn.Linear(in_features=2048, out_features=1)
+        self.model.load_state_dict(torch.load('ml/model/model-resnet50.pth', map_location=self.device)) 
+        self.model.eval().to(self.device)
+
+    def prepare_image(self, image):
+        if image.mode != 'RGB':
+            image = image.convert("RGB")
+        Transform = transforms.Compose([
+                    transforms.Resize([224,224]),      
+                    transforms.ToTensor(),
+                ])
+        image = Transform(image)   
+        image = image.unsqueeze(0)
+        return image.to(self.device)
+
+    def predict(self, image):
+        image = self.prepare_image(image)
+        with torch.no_grad():
+            preds = self.model(image)
+        return preds.item()
